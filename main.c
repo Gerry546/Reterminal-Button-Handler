64c5fe2b1103913415ab60953d2b25e22e297e0c
Tom Geelen <t.f.g.geelen@gmail.com>
2023-06-29 19:00:34 +0000

    added initial meson build and button handler

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
-//
-// main is where all program execution starts
-//
-int main(int argc, char **argv) {
-  printf("Hello there.\n");
-  return 0;
+void INThandler()
+{
+  exit(0);
+}
+
+int main()
+{
+  char devname[] = "/dev/input/event2";
+  int device = open(devname, O_RDONLY);
+  struct input_event ev;
+
+  signal(SIGINT, INThandler);
+
+  while (1)
+  {
+    read(device, &ev, sizeof(ev));
+    if (ev.type == 1 && ev.value == 1)
+    {
+      printf("Key: %i State: %i\n", ev.code, ev.value);
+    }
+  }
 }
